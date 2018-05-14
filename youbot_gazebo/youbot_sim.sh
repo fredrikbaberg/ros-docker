3d85eb04f496ef4709d12a3aaed00a456fbb7016
Fredrik Baberg <fbaberg@kth.se>
2018-05-14 11:44:50 +0200

    Skip xvfb, should work anyway.

--- youbot_gazebo/youbot_sim.sh
+++ youbot_gazebo/youbot_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Xvfb :1 -screen 0 1600x1200x16 &
-export DISPLAY=:1.0
+# Xvfb :1 -screen 0 1600x1200x16 &
+# export DISPLAY=:1.0
 echo "Master URI: $MASTER_URI"
 if [ ! -z "${MASTER_URI}" ]; then
     echo "Exporting URI"
